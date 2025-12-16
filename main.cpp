d3d1ac9486cc0025e263de6d5c3c5e919d316f5d
naufal <edginanaufalapta@gmail.com>
2025-12-16 09:39:20 +0700

    penambahan data dummy pada main.cpp
    Removed unnecessary comments for sales sections.

--- main.cpp
+++ main.cpp
@@ -2,23 +2,15 @@
 
 using namespace std;
 
-/* ============================================================
-   VARIABEL GLOBAL
-   ============================================================ */
 ListSales L;
 
-/* ============================================================
-   MAIN PROGRAM
-   ============================================================ */
 int main() {
     createListSales(L);
 
     addressSales S;
     addressMobil M;
 
-    // =========================
     // SALES 1
-    // =========================
     infotypeSales s1 = {"S001", "Andi Wijaya", "081234567890"};
     S = createElmSales(s1);
     insertLastSales(L, S);
@@ -30,9 +22,7 @@ int main() {
     M = createElmMobil({"Daihatsu", "Xenia", "2023", "M003", 300000000, 4});
     insertLastMobil(S, M);
 
-    // =========================
     // SALES 2
-    // =========================
     infotypeSales s2 = {"S002", "Budi Santoso", "082345678901"};
     S = createElmSales(s2);
     insertLastSales(L, S);
@@ -42,9 +32,7 @@ int main() {
     M = createElmMobil({"Mitsubishi", "Pajero", "2021", "M005", 550000000, 2});
     insertLastMobil(S, M);
 
-    // =========================
     // SALES 3
-    // =========================
     infotypeSales s3 = {"S003", "Citra Lestari", "083456789012"};
     S = createElmSales(s3);
     insertLastSales(L, S);
@@ -56,9 +44,7 @@ int main() {
     M = createElmMobil({"Suzuki", "XL7", "2021", "M008", 290000000, 4});
     insertLastMobil(S, M);
 
-    // =========================
     // SALES 4
-    // =========================
     infotypeSales s4 = {"S004", "Dewi Anggraini", "084567890123"};
     S = createElmSales(s4);
     insertLastSales(L, S);
@@ -68,9 +54,7 @@ int main() {
     M = createElmMobil({"Toyota", "Rush", "2020", "M010", 320000000, 2});
     insertLastMobil(S, M);
 
-    // =========================
     // MENU
-    // =========================
     menuAdmin(L);
 
     cout << "\nProgram selesai.\n";
