59c1aabf62bab060b9083d6795521e8b9f28fe06
naufal <edginanaufalapta@gmail.com>
2025-12-16 09:44:55 +0700

    Update header.h
    Added function declarations for menu operations and updated comments for clarity.

--- header.h
+++ header.h
@@ -5,6 +5,9 @@
 #include <string>
 using namespace std;
 
+/* ============================================================
+   MACRO
+   ============================================================ */
 #define next(P) P->next
 #define prev(P) P->prev
 #define info(P) P->info
@@ -13,7 +16,7 @@ using namespace std;
 typedef struct elmList_Sales* addressSales;
 
 /* ============================================================
-   BAGIAN MOBIL
+   BAGIAN MOBIL (Child DLL)
    ============================================================ */
 struct infotypeMobil {
     string merk;
@@ -32,23 +35,20 @@ struct elmList_Mobil {
     addressMobil prev;
 };
 
+/* ---- FUNGSI MOBIL ---- */
 addressMobil createElmMobil(infotypeMobil data);
-
 void insertFirstMobil(addressSales S, addressMobil M);
 void insertLastMobil(addressSales S, addressMobil M);
 void insertAfterMobil(addressSales S, addressMobil Prec, addressMobil M);
-
 void deleteFirstMobil(addressSales S, addressMobil &M);
 void deleteLastMobil(addressSales S, addressMobil &M);
 void deleteAfterMobil(addressSales S, addressMobil Prec, addressMobil &M);
-
 void deleteMobilFromSales(addressSales S, string idMobil);
-
 addressMobil findMobilPadaSales(addressSales S, string idMobil);
 void viewMobilSales(addressSales S);
 
 /* ============================================================
-   BAGIAN SALES 
+   BAGIAN SALES (Parent SLL)
    ============================================================ */
 struct infotypeSales {
     string idSales;
@@ -59,8 +59,6 @@ struct infotypeSales {
 struct elmList_Sales {
     infotypeSales info;
     addressSales next;
-
-    // Relasi ke mobil
     addressMobil child;
 };
 
@@ -68,22 +66,25 @@ struct ListSales {
     addressSales first;
 };
 
-
+/* ---- FUNGSI SALES ---- */
 void createListSales(ListSales &L);
 addressSales createElmSales(infotypeSales data);
-
 void insertFirstSales(ListSales &L, addressSales P);
 void insertLastSales(ListSales &L, addressSales P);
 void insertAfterSales(ListSales &L, addressSales Prec, addressSales P);
-
 void deleteFirstSales(ListSales &L, addressSales &P);
 void deleteLastSales(ListSales &L, addressSales &P);
 void deleteAfterSales(ListSales &L, addressSales Prec, addressSales &P);
-
 addressSales findElemenSales(ListSales L, string idSales);
 void viewSales(ListSales L);
-
 void hitungMobilTerjualPerMerk(ListSales L);
 void hitungScoreSales(ListSales L);
+
+/* ============================================================
+   MENU
+   ============================================================ */
+void menuAdmin(ListSales &L);
+void menuParent(ListSales &L);
+void menuChild(ListSales &L);
 
 #endif // HEADER_H_INCLUDED
